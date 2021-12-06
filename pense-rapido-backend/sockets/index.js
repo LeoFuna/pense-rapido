9e34e17ab425ab3e406c42171827d9010a7cff6d
LeoFuna <leo.funa@gmail.com>
2021-12-06 14:47:23 -0300

    feat: ideia de como funcionara o game nos socket.io implementadas

--- pense-rapido-backend/sockets/index.js
+++ pense-rapido-backend/sockets/index.js
@@ -2,6 +2,11 @@ module.exports = (io) =>
   io.on("connection", (socket) => {
     socket.on("squarePress", async ({ playerId }) => {
       console.log(`O jogador de id: ${playerId}, marcou 1 ponto`);
-      // io.emit("refreshScore", data);
+      // io.emit("updateSquarePosition") aqui deve atualizar a posição do quadrado para outro local de forma aleatória
+      // io.emit("refreshScore", {  }); aqui o io deve emitir uma atualização para o score do jogador que clicou primeiro para o front
+    });
+    socket.on("startGame", async () => {
+      console.log('o Jogo foi iniciado!')
+      // io.emit("updateSquarePosition") aqui vai chamar esse 'canal' para que apareça um quadrado na tela em local aleatório
     });
   });
